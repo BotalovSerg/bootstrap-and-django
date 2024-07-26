08877dd5a73788d4b7e7b58c39fef00e846ea80f
botalov <89090168690@mail.ru>
2024-07-26 14:13:39 +0500

    update index.html

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from .models import Book, Author, BookInstance
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    text_head = "Это заголовк главной страницы"
-    text_body = "This is content main page"
-    context = {"text_head": text_head, "text_body": text_body}
+    text_head = "На нашем сайте вы можете получить книги в электронном виде"
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    authors = Author.objects.all()
+    num_authors = Author.objects.all().count()
+
+    context = {
+        "text_head": text_head,
+        "books": books,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "authors": authors,
+        "num_authors": num_authors,
+    }
     return render(request, "catalog/index.html", context=context)
