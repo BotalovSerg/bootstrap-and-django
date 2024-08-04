f92b592b0c6d375491f2e378a86cfa6d618b0ca7
Botalov Serg <89090168690@mail.ru>
2024-08-04 22:43:59 +0500

    add postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'site_db',
+        'USER': 'botalov',
+        'PASSWORD': 'botalov',
+        'HOST': 'localhost',
+        'PORT': 5430,
     }
 }
 
